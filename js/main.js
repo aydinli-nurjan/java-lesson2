f9ef9f77722a2c5f138acf0f80b3e88f78d97385
Khadija Aydinli <khadijaaydinligmail.com@Khadijas-MacBook-Air.local>
2024-07-09 11:24:24 +0400

    homework

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let age = 15
+let Name = "Nurjan"
+let lastName = "Aydinli"
+let hobby = "listening music"
+
+console.log("Hello my name is " + Name + ". And my lastname is " + lastName + ". I am " + age + " years old" + ". I spend a lot of time to " + hobby + ". This is one of my favorite things to do.")
